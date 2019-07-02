fa8e8b253ebd771f99c41554ed44b80197a23ba7
Brent Roose <brent@spatie.be>
2019-07-02 14:45:11 +0200

    Ignore Laravel files

--- preload.php
+++ preload.php
@@ -109,6 +109,12 @@ class Preloader
 
 (new Preloader())
     ->paths(__DIR__ . '/vendor/laravel')
-    ->ignore(\Illuminate\Filesystem\Cache::class)
+    ->ignore(
+        \Illuminate\Filesystem\Cache::class,
+        \Illuminate\Log\LogManager::class,
+        \Illuminate\Http\Testing\File::class,
+        \Illuminate\Http\UploadedFile::class,
+        \Illuminate\Support\Carbon::class,
+    )
     ->load();
 
